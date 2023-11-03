f4e6a52d90ef3ec8aae334e0796e06bb2485d30e
Juan David Anzola Quiroga <jdanzola@uninorte.edu.co>
2023-11-03 18:54:29 +0000

    params structured and first directory finder

--- app/generador.py
+++ app/generador.py
@@ -1,17 +1,52 @@
 
 import os
 import networkx as nx 
+import getopt
+import sys
+import glob 
+import time 
 
-def find_files(directory, filename):
-    """
-    Busca archivos con el nombre especificado en el directorio y sus subdirectorios.
-    Devuelve una lista con las rutas de los archivos encontrados.
-    """
-    files = []
-    for root, _, filenames in os.walk(directory):
-        for file in filenames:
-            if file == filename:
-                files.append(os.path.join(root, file))
-    return files
+#making a measure of the time execution of the program
+start_time = time.time()
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "d:fi:ff:h:grt:jrt:gm:jm:gcrt:jcrt")
+
+    for o,a in opts:
+        if o == "-d":
+            print("Directorio: ", a)
+        elif o == "-fi":
+            print("Fecha inicial: ", a)
+        elif o == "-ff":
+            print("Fecha final: ", a)
+        elif o == "-h":
+            print("Nombre de archivo ", a) #Hastag
+        elif o == "-grt":
+            print("Grafo de retweets: ", a)
+        elif o == "-jrt":
+            print("JSON de retweets: ", a)
+        elif o == "-gm":
+            print("Grafo de menciones: ", a)
+        elif o == "-jm":
+            print("JSON de menciones: ", a)
+        elif o == "-gcrt":
+            print("Grafo de componentes retweets: ", a)
+        elif o == "-jcrt":
+            print("JSON de componentes retweets: ", a)
+        else:
+            assert False, "unhandled option"
+
+except getopt.GetoptError as err:
+    # print help information and exit:
+    print(err)  # will print something like "option -a not recognized"
+    sys.exit(2)
+
+
+# Finding x.json.bz2 files in a directory where x is a number 
+print(glob.glob("*/*.json.bz2"))
+end_time = time.time()
+print("Time execution: ", end_time - start_time)
+
+
 
 
