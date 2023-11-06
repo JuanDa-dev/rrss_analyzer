1abc5818e85228a24dbdecbf6336fd51a487eb5a
Juan Anzola <jdanzolaq@uninorte.edu.co>
2023-11-06 11:27:00 -0500

    Decompressing json.bz2 file with errors

--- app/generador.py
+++ app/generador.py
@@ -1,13 +1,16 @@
 
 import os
 import networkx as nx 
-import getopt
-import sys
 import glob 
-import time 
+import json 
+import shutil,bz2,getopt,sys
+from collections import defaultdict
+import bz2 
 
-#making a measure of the time execution of the program
-start_time = time.time()
+# Get command line arguments
+fecha_inicial = None
+fecha_final = None
+hashtag = None
 
 try:
     opts, args = getopt.getopt(sys.argv[1:], "d:fi:ff:h:grt:jrt:gm:jm:gcrt:jcrt")
@@ -41,11 +44,38 @@ except getopt.GetoptError as err:
     print(err)  # will print something like "option -a not recognized"
     sys.exit(2)
 
+# Looking for json.bz2 files in the directory
+file_list = glob.glob("*/*.json.bz2")
 
-# Finding x.json.bz2 files in a directory where x is a number 
-print(glob.glob("*/*.json.bz2"))
-end_time = time.time()
-print("Time execution: ", end_time - start_time)
+# Decompressing in json file 
+for file_path in file_list:
+    with bz2.open(file_path, "rb") as input_file:
+        # Read the compressed data
+        compressed_data = input_file.read()
+        
+        # Get the output file name by removing the ".bz2" extension
+        output_file_name = os.path.splitext(file_path)[0]
+        
+        # Write the decompressed data to the output file
+        with open(output_file_name, "w") as output_file:
+            output_file.write(compressed_data.decode("utf-8"))
+
+
+
+
+
+# #Filtter by fi and ff 
+# if opts[1] == "-fi":
+#     file_list = [x for x in file_list if x.split("/")[0] >= opts[2]]
+# if opts[3] == "-ff":
+#     file_list = [x for x in file_list if x.split("/")[0] <= opts[4]]
+# else:
+#     pass
+
+
+
+
+
 
 
 
